b6e5ffaab8f78855782cacdb83ffa7adf46e1fe6
Moustaoui Salaheddine <moustaoui.salaheddine@gmail.com>
2022-11-16 00:25:52 +0100

    init project

---
+++ src/test/java/com/mst/terrain/TerrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst.terrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
