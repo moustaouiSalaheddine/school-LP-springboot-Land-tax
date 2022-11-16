c54651fb5bda56fba3540e7aa9b66b7b50b2c4bb
Moustaoui Salaheddine <moustaoui.salaheddine@gmail.com>
2022-11-16 01:22:55 +0100

    create empty dao's

---
+++ src/main/java/com/mst/terrain/dao/TauxTnbDao.java
@@ -0,0 +1,9 @@
+package com.mst.terrain.dao;
+
+import com.mst.terrain.bean.TauxTnb;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TauxTnbDao extends JpaRepository<TauxTnb, Long> {
+}
